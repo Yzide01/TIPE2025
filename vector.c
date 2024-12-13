cfd3483e05a874fde4e4f9b6c5faeaa9cd80d098
Yzide01 <115028987+Yzide01@users.noreply.github.com>
2024-12-13 15:02:47 +0100

    Update vector.c

--- vector.c
+++ vector.c
@@ -1,1 +1,36 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "vector.h"
+
+//Initialise un vecteur de taille n au vecteur nul
+vector create_zero_vector (int n){
+    vector v = malloc(sizeof(struct vector_s));
+    v->taille = n;
+    v->content = malloc(n * sizeof(unsigned char));
+    for (int i = 0; i < n; i++){
+        v->content[i] = 0;
+    }
+    return v;
+}
+
+//Libère l'espace mémoire allouée pour v
+void delete_vector (vector v){
+    free(v->content);
+    free(v);
+}
+
+//Affiche le vecteur v sous le format (v1, v2, ..., vn)
+void print_vector (vector v){
+    int n = v->taille;
+    printf("(");
+    for (int i = 0; i < n; i++){
+        printf("%d",v->content[i]);
+        if (i != n-1){
+            printf(", ");
+        }
+    }
+    printf(")\n");
+}
+
+
